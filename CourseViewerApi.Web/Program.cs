bb53cd079c0ef76cbf8eacbfe57f6b0b76699126
elkprostoelk <novikovmisha138138@gmail.com>
2023-06-01 21:50:54 +0300

    provided docker config

--- CourseViewerApi.Web/Program.cs
+++ CourseViewerApi.Web/Program.cs
@@ -24,11 +24,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 var logger = app.Logger;
 app.UseExceptionHandler(appBuilder => appBuilder.Run(async context =>
